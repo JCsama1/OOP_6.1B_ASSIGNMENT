e355d3ae6cc50121abdc9a08d27ca0b63cc88ec1
JCsama1 <james.cutajar.e21616@mcast.edu.mt>
2022-01-17 17:47:59 +0100

    Bird Script, Background and Collisions

---
+++ James_Cutajar/Assets/Scripts/Bird.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    Vector2 startPosition;
+    Rigidbody2D _rigidbody2D;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = _rigidbody2D.position;
+        _rigidbody2D.isKinematic = true;
+    }
+
+    void OnMouseDown()
+    {
+        spriteRenderer.color = Color.red;
+    }
+
+    void OnMouseUp()
+    {
+        Vector2 currentPosition = _rigidbody2D.position;
+        Vector2 direction = startPosition - currentPosition;
+        direction.Normalize();
+
+        _rigidbody2D.isKinematic = false;
+        _rigidbody2D.AddForce(direction * 500);
+
+        spriteRenderer.color = Color.white;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+    }
+}
