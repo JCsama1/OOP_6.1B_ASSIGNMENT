3014c3f273b14c06cfdcf1b516fe4e59a91b4641
JCsama1 <james.cutajar.e21616@mcast.edu.mt>
2022-01-19 15:39:30 +0100

    monster contents and drag

--- James_Cutajar/Assets/Scripts/Bird.cs
+++ James_Cutajar/Assets/Scripts/Bird.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+
+    [SerializeField] float launchForce = 500;
+    [SerializeField] float maxDragDistance = 5;
+
+
     Vector2 startPosition;
     Rigidbody2D _rigidbody2D;
     SpriteRenderer spriteRenderer;
@@ -33,7 +39,7 @@ public class Bird : MonoBehaviour
         direction.Normalize();
 
         _rigidbody2D.isKinematic = false;
-        _rigidbody2D.AddForce(direction * 500);
+        _rigidbody2D.AddForce(direction * launchForce);
 
         spriteRenderer.color = Color.white;
     }
@@ -41,6 +47,34 @@ public class Bird : MonoBehaviour
     void OnMouseDrag()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+        //limit to drag
+        Vector2 desiredPosition = mousePosition;
+
+        float distance = Vector2.Distance(desiredPosition, startPosition);
+        if (distance > maxDragDistance)
+        {
+            Vector2 direction = desiredPosition - startPosition;
+            direction.Normalize();
+            desiredPosition = startPosition + (direction * maxDragDistance);
+        }
+
+        if (desiredPosition.x > startPosition.x)
+            desiredPosition.x = startPosition.x;
+
+        _rigidbody2D.position = desiredPosition;
+    }
+
+    //reset bird position
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(ResetAfterDelay());
+    }
+
+    IEnumerator ResetAfterDelay()
+    {
+        yield return new WaitForSeconds(3);
+        _rigidbody2D.position = startPosition;
+        _rigidbody2D.isKinematic = true;
+        _rigidbody2D.velocity = Vector2.zero;
     }
 }
---
+++ James_Cutajar/Assets/Scripts/Monster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField] Sprite deadSprite;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (ShouldDieFromCollision(collision))
+        {
+            Die();
+        }
+    }
+
+    bool ShouldDieFromCollision(Collision2D collision)
+    {
+        Bird bird = collision.gameObject.GetComponent<Bird>();
+        if (bird != null)
+            return true;
+
+        if (collision.contacts[0].normal.y < -0.5)
+            return true;
+
+        return false;
+    }
+
+    void Die()
+    {
+       GetComponent<SpriteRenderer>().sprite = deadSprite;
+       //gameObject.SetActive(false);
+    }
+}
